9937a3d01ac21ff0c46095c311421822dc18b2c9
retief <retief.visser@fivium.com.au>
2017-09-11 14:00:46 +1000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', message => {
     console.log('New message to create', message);
     io.emit('newMessage', {
