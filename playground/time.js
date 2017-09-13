d22ca0bbaf8d98bb5f2a4eabe793edd1a79bb1de
retief <retief.visser@fivium.com.au>
2017-09-13 14:57:06 +1000

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,15 @@
+const moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(1, 'year').subtract(9, 'months');
+// console.log(date.format('h:mm a'));
+
+// console.log(moment().utcOffset(10).format());
+
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,13 +1,15 @@
+const moment = require('moment');
+
 var generateMessage = (from, text) => ({
   from,
   text,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 
 var generateLocationMessage = (from, latitude, longitude) => ({
   from,
   url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 
 module.exports = {generateMessage, generateLocationMessage};
